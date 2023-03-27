ef2a95a802e4c9426bba168b0f1be2cadd68979e
Syaifal Illahi <garethbalefal@gmail.com>
2023-03-27 17:58:46 +0700

    perbaikan sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function App() {
       {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
       {/* <SearchBooking /> */}
       {/* <SidebarBooking /> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
     </main>
   )
 }
